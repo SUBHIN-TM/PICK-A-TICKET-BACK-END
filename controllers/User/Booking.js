935975ce5ed417c06d8d5ee763e63f2b2ef5ce73
SUBHIN-TM <subhintm2018@gmail.com>
2024-06-07 21:43:16 +0530

    seat book using for loop

--- controllers/User/Booking.js
+++ controllers/User/Booking.js
@@ -23,7 +23,10 @@ export const booking = async (req, res) => {
         date: date,
         time: time,
         screen: screen,
-        totalSeats:{0:null,1:null,2:"booked",3:null,4:null,5:null,6:null,7:null,8:null,9:null,10:null,11:null,12:null},
+        totalSeats:{0:null,1:null,2:"booked",3:null,4:null,5:null,6:null,7:null,8:null,9:null,10:null,11:null,12:null,
+          13:null,14:null,15:"booked",16:null,17:null,18:null,19:null,20:null,21:null,22:null,23:null,24:null,25:null,
+          26:null,27:null,28:null,29:null,30:null
+        },
         bookingDetails: []
       });
 
