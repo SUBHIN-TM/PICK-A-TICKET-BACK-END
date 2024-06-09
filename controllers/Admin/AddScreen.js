4dfbe8eae3dc9b3fcfc33a01fd3576fb20e74417
SUBHIN-TM <subhintm2018@gmail.com>
2024-06-09 13:52:19 +0530

    booking route need to be impliment

--- controllers/Admin/AddScreen.js
+++ controllers/Admin/AddScreen.js
@@ -4,10 +4,10 @@ export const AddScreens=async()=>{
     try {
         console.log("add screen section");
         const newScreen = new SCREEN({
-            screen: 'SCREEN 2',
-            movie: 'Anchakkallakokkan',
+            screen: 'SCREEN 1',
+            movie: 'Manjummal Boys',
             totalSeat: 100,
-            times: ["10:00 AM","1:30PM","7:00 PM", "10:00 PM"]
+            times: ["7:00 PM","10:00PM"]
         });
 
         const response = await newScreen.save();
