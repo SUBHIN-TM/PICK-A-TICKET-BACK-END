ce13239470aa04599b434d92b7fa12654a23dee1
SUBHIN-TM <subhintm2018@gmail.com>
2024-06-09 13:29:08 +0530

    seatarranged

--- controllers/Admin/AddScreen.js
+++ controllers/Admin/AddScreen.js
@@ -4,10 +4,10 @@ export const AddScreens=async()=>{
     try {
         console.log("add screen section");
         const newScreen = new SCREEN({
-            screen: 'screen 2',
+            screen: 'SCREEN 2',
             movie: 'Anchakkallakokkan',
             totalSeat: 100,
-            times: ["10:00 AM","1:30","7:00 PM", "10:00 PM"]
+            times: ["10:00 AM","1:30PM","7:00 PM", "10:00 PM"]
         });
 
         const response = await newScreen.save();
